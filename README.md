8a639e6fb3c4f7fde77db9c6bef1ae3fd8d415b2
Zielinski <szielins@net.ist.ucf.edu>
2019-01-29 19:05:38 -0500

    Removed meta files as these are temp save files that unity uses while you are running the project. Updated the readme to include the new information that I have finished. Added the SteamVR package and confirmed that it is working in the menu scene. Started working on the script to advance from the menu screen to the other scenes.

--- README.md
+++ README.md
@@ -3,6 +3,9 @@ COP 6121
 
 Steven Work Done 
     - Created the main menu
+        - Added in the background
+        - Added in the two buttons
+        - Added the steam vr and hooked it up with unity
     - Found asset for battle droid
         - Made changes for battle droid to fix the texturing
         - This was a free asset created by SmeagolGollum
---
+++ StarWarsVR/Assets/Scripts/MainMenu/LaunchScene.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaunchScene : MonoBehaviour
+{
+	private SteamVR_TrackedContoller controller;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = gameObject.getComponent<SteamVR_TrackedController>();
+    }	
+
+    // Update is called once per frame
+    void Update()
+    {
+    		if (controller.triggerPressed)
+		{
+			RayCast	hit;
+			
+			if (Physics.Raycast(new Ray(gameObject.transform.position, gameObject.transform.forward), out hit))
+            {
+                if (hit.collider.name.Equals("startButton"))
+                {
+                    hit.collider.gameObject.GetComponent<MenuStartButton>().LoadGameScene();
+                }
+            }
+		}   
+    }
+}
